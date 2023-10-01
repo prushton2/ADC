d8c59a2a172baaa6b948901444f76805847c5bf3
prushton2 <46426283+prushton2@users.noreply.github.com>
2023-09-30 22:24:26 -0400

    Moving to a state-oriented player movement

--- Assets/Player/Scripts/Player.cs
+++ Assets/Player/Scripts/Player.cs
@@ -21,26 +21,82 @@ public class Player : MonoBehaviour
 
     //control
 
+    //grounded, inair;
+    public string state = "grounded";
+    public int stateProgress = 0;
     public bool movementLocked = false;
 
     //computational
 
     private Vector3 moveDirection = Vector3.zero;
+    private Vector3 hardVelocity = Vector3.zero;
 
     void Start() {
         characterController = GetComponent<CharacterController>();
     }
 
+    void FixedUpdate() {
+        //we update the state
+        
+
+        //we put physics critical things here, like hard velocity falloff
+        switch(state) {
+            case "grounded":
+                hardVelocityFallOff(30);
+                break;
+            case "inair":
+                hardVelocityFallOff(1);
+                break;
+        }   
+        characterController.Move(hardVelocity);
+
+    }
+
     void Update()
     {
-
         if(movementLocked) { //needs to be changed sometime soon (its cringe af)
             return;
         }
 
+        //update this here instead because fast
+        if(state == "grounded" || state == "inair") {
+            state = characterController.isGrounded ? "grounded" : "inair";
+            Debug.Log(characterController.isGrounded);
+        }
+
+        //We put user-critical things here, things the user controls. This has a higher refresh rate, and the movement is accurate at low frame rates
+        switch (state) {
+            case "grounded":
+                look();
+                wasd();
+                jump();
+                fall();
+                break;
+            
+            case "inair":
+                look();
+                wasd();
+                fall();
+                break;
+
+            case "dash":
+                look();
+                dash();
+                break;
+
+            default:
+                break;
+        }
+
+        characterController.Move(moveDirection); //ALL REFERENCES TO MOVEDIRECTION SHOULD INCLUDE TIME.DELTATIME
+    }
+
+    void look() {
         deltaX += Input.GetAxisRaw("Mouse X");
         transform.rotation = Quaternion.Euler(0, Sensitivity*deltaX, 0);
+    }
 
+    void wasd() {
         moveDirection.x = 0;
         moveDirection.z = 0;
 
@@ -61,29 +117,51 @@ public class Player : MonoBehaviour
             moveDirection.z += -transform.right.z*speed*Time.deltaTime;
         }
         
-        if(Input.GetKey("left shift")) {
-            moveDirection = new Vector3(moveDirection.x * runMultiplier, moveDirection.y, moveDirection.z * runMultiplier);
+        if(Input.GetKeyDown("left shift")) {
+            state = "dash"; //moveDirection = new Vector3(moveDirection.x * runMultiplier, moveDirection.y, moveDirection.z * runMultiplier);
+            stateProgress = 0;
         }
 
-        characterController.Move(moveDirection);
+    }
 
+    void fall() {
         if (!characterController.isGrounded) { //This code modifies the accumulator moveDirection.y, allowing jumping and falling properly
             moveDirection.y -= gravity*Time.deltaTime;
         } else {// (characterController.isGrounded) {
             moveDirection.y = 0;
-
-            if(Input.GetKey("space")) {
-                moveDirection.y = jumpSpeed;
-            }
         }
+    }
+
+    void jump() {
+        if(Input.GetKey("space")) {
+            moveDirection.y = jumpSpeed;
+        }
+    }
+
+    void hardVelocityFallOff(int rate) {
+        float drate = rate * 0.01f;
+        hardVelocity -= new Vector3(drate * hardVelocity.x, drate * hardVelocity.y, drate * hardVelocity.z);
+    }
+
+    void dash() {
+        if(Input.GetKey("space")) {
+            jump();
+            state = "inair";
+            stateProgress = 0;
+            hardVelocity = new Vector3(hardVelocity.x * 2, hardVelocity.y * 2, hardVelocity.z * 2);
+        }
+        stateProgress += 1;
+        if(stateProgress == 1) {
+            hardVelocity = transform.forward;
+        }
+
+        if(stateProgress >= 200) {
+            hardVelocity = Vector3.zero;
+            state = "grounded";
+            stateProgress = 0;
+        }
+    }
 
         
 
-
-
-    }
-
-    void FixedUpdate() {}
-
 }
-
