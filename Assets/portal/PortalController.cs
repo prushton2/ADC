2b23ffcfc9ada931ac3970467de17158ccc76e60
prushton2 <46426283+prushton2@users.noreply.github.com>
2023-10-17 17:52:08 -0400

    added a third portal and created an unlink function

--- Assets/portal/PortalController.cs
+++ Assets/portal/PortalController.cs
@@ -18,9 +18,13 @@ public class PortalController : Executable
     // public GameObject Portal3;
 
 
-    void Start()
+    void FixedUpdate()
     {
-        setLink(LinkA, LinkB);
+        if(Input.GetKey("i")) {
+            setLink(LinkA, LinkB);
+        } else if (Input.GetKey("o")) {
+            unlink(LinkA, LinkB);
+        }
     }
 
     void setLink(GameObject portalA, GameObject portalB) {
@@ -52,6 +56,27 @@ public class PortalController : Executable
         //Link the teleporter scripts
         portalA.transform.GetChild(3).gameObject.GetComponent<portalTeleporter>().otherPortal = portalB;
         portalB.transform.GetChild(3).gameObject.GetComponent<portalTeleporter>().otherPortal = portalA;
+    }
+
+    void unlink(GameObject portalA, GameObject portalB) {
+        //set the other portals
+        portalA.transform.GetChild(0).gameObject.GetComponent<portalcamera>().otherPortal = null;
+        portalB.transform.GetChild(0).gameObject.GetComponent<portalcamera>().otherPortal = null;
+                
+        //reference cameras
+        Camera camA = portalA.transform.GetChild(0).gameObject.GetComponent<Camera>();
+        Camera camB = portalB.transform.GetChild(0).gameObject.GetComponent<Camera>();
+        //remove any target textures they may have
+        if(camA.targetTexture != null) { camA.targetTexture.Release(); }
+        if(camB.targetTexture != null) { camB.targetTexture.Release(); }
+        //set the camA's target texture to a new render texture based on the screen size and set Camera Mat A's texture to the render texture
+
+        //set the portal's plane's material to the other portals material
+        portalA.transform.GetChild(2).gameObject.GetComponent<Renderer>().material = null;
+        portalB.transform.GetChild(2).gameObject.GetComponent<Renderer>().material = null;
+        //Link the teleporter scripts
+        portalA.transform.GetChild(3).gameObject.GetComponent<portalTeleporter>().otherPortal = null;
+        portalB.transform.GetChild(3).gameObject.GetComponent<portalTeleporter>().otherPortal = null;
     }
 
     public override string execute(string[] args) {
